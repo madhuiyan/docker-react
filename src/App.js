c75b83f6fb7bcfe88f0a3229fdb11795b735cd32
madhuiyan <madhu.iyanperumal@gmail.com>
2023-07-05 07:26:47 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>  Work Is Worship. Learning is Living
         </p>
         <a
           className="App-link"
